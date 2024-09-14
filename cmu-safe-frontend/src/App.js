b9e62fbe3c886a673c16c489bcf688279e1c92da
axgu <annaxigu@gmail.com>
2024-09-14 01:05:22 -0400

    fix form submission

--- cmu-safe-frontend/src/App.js
+++ cmu-safe-frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleMap from './GoogleMap.js';
 import { useState } from 'react';
 
 function App() {
   const [result, setResult] = useState('');
 
-  const runPythonScript = async () => {
+  const runPythonScript = async (origin, destination) => {
     const response = await fetch('http://localhost:5000/run-python', {
       method: 'POST',
       headers: {
@@ -13,7 +14,7 @@ function App() {
       },
       body: JSON.stringify({
         scriptName: '../cmu-safe-backend/main.py',  // Path to Python script in backend
-        args: ['arg1', 'arg2'],
+        args: [origin, destination],
       }),
     });
 
@@ -21,12 +22,33 @@ function App() {
     setResult(data);
   };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = {
+      origin: form.origin.value, // Access value of the input field with name="name"
+      dest: form.destination.value, // Access value of the input field with name="email"
+    };
+
+    runPythonScript(formData.origin, formData.dest);
+  };
+
   return (
     <div className="App">
-      <button onClick={runPythonScript}>Run Python Script</button>
-      <p>Result: {result}</p>
+      <h1>CMU Safe</h1>
+      <div className="Form">
+        <form onSubmit={onSubmit}>
+          <label>Origin: </label>
+          <input type="text" id="origin" name="origin"></input><br></br>
+          <label>Destination: </label>
+          <input type="text" id="destination" name="destination"></input><br></br>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+      <GoogleMap />
     </div>
   );
+
 }
 
 export default App;
--- cmu-safe-frontend/src/GoogleMap.js
+++ cmu-safe-frontend/src/GoogleMap.js
@@ -1,6 +1,7 @@
-
+"use client";
 import React, { useEffect, useState, useRef, useCallback } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; 
+// import { useGoogleMaps } from 'react-hook-google-maps';
 import {
   APIProvider,
   Map,
@@ -21,6 +22,7 @@ const locations = [
   { key: 'nevilleHouse', location: { lat: 40.44745632543893, lng: -79.94727836996164 } },
 ];
 
+
 const PoiMarkers = (props) => {
   return (
     <>
@@ -36,7 +38,6 @@ const PoiMarkers = (props) => {
 };
 
 function GoogleMap() {
-  
   return (
     <APIProvider apiKey={API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
       <Map
@@ -48,10 +49,46 @@ function GoogleMap() {
           console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
         }>
         <PoiMarkers pois={locations} />
+        <Directions />
       </Map>
     </APIProvider>
   );
 
 }
+
+function Directions() {
+  const map = useMap();
+  const routesLibrary = useMapsLibrary("routes");
+  const mapsLibrary = useMapsLibrary("traveling");
+  const [directionsService, setDirectionsService] = useState();
+  const [directionsRenderer, setDirectionsRenderer] = useState();
+  const [routes, setRoutes] = useState([]);
+
+  useEffect(() => {
+    if (!routesLibrary || !map) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({map}));
+  }, [routesLibrary, map]);
+ 
+ 
+  useEffect(() => {
+    if (!directionsService || !directionsRenderer) return;
+ 
+ 
+    directionsService.route({
+      origin: "4614 Fifth Ave, Pittsburgh, PA 15213",
+      destination: "Gates Hillman Complex, 4902 Forbes Ave, Pittsburgh, PA 15213",
+      travelMode: window.google.maps.TravelMode.DRIVING,
+      provideRouteAlternatives: true,
+    }).then(response => {
+      directionsRenderer.setDirections(response);
+      setRoutes(response.routes)
+    });
+  }, [directionsService, directionsRenderer]);
+ 
+ 
+  // console.log(routes);
+  return null;
+} 
 
 export default GoogleMap;
