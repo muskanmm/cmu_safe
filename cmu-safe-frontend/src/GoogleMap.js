b3a24cef49c7a3ec0352e44682f9b766f8aa6b39
axgu <annaxigu@gmail.com>
2024-09-13 23:03:39 -0400

    map frontend

---
+++ cmu-safe-frontend/src/GoogleMap.js
@@ -0,0 +1,57 @@
+
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  APIProvider,
+  Map,
+  Marker,
+  AdvancedMarker,
+  MapCameraChangedEvent,
+  useMap,
+  useMapsLibrary,
+  Pin
+} from '@vis.gl/react-google-maps';
+// import {MarkerClusterer} from '@googlemaps/markerclusterer';
+
+const globals = require("./front-end-globals");
+const API_KEY = globals.API_KEY;
+const MAP_ID = globals.MAP_ID;
+
+const locations = [
+  { key: 'nevilleHouse', location: { lat: 40.44745632543893, lng: -79.94727836996164 } },
+];
+
+const PoiMarkers = (props) => {
+  return (
+    <>
+      {props.pois.map((poi) => (
+        <Marker
+          key={poi.key}
+          position={poi.location}>
+          <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+        </Marker>
+      ))}
+    </>
+  );
+};
+
+function GoogleMap() {
+  
+  return (
+    <APIProvider apiKey={API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+      <Map
+        style={{ width: '100vw', height: '100vh' }}
+        mapIDs={MAP_ID}
+        defaultZoom={13}
+        defaultCenter={{ lat: 40.44745632543893, lng: -79.94727836996164 }}
+        onCameraChanged={(ev) =>
+          console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+        }>
+        <PoiMarkers pois={locations} />
+      </Map>
+    </APIProvider>
+  );
+
+}
+
+export default GoogleMap;
